d942713c4eccfbe3e134f1914b985da245351296
Andrii Kryvyi <87859325+Ronelos@users.noreply.github.com>
2022-01-23 20:09:33 +0200

    hw11 -1

---
+++ src/api-service.js
@@ -0,0 +1,1 @@
+const BASEURL = 'https://pixabay.com/api/?key=25377034-83ee895c314302b75a7ed46d6';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import CARD from './partials/card.hbs';
+import CARD_BOX from './partials/card_box.hbs';
+
+
+
